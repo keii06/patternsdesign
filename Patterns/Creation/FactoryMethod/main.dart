f7629e464866888fa38844f4a943fd739e7b46f1
keii06 <k.eiguar1997@gmail.com>
2022-09-12 15:12:05 -0600

    FactoryMethod

--- Patterns/Creation/FactoryMethod/main.dart
+++ Patterns/Creation/FactoryMethod/main.dart
@@ -1,1 +1,36 @@
-void main() {}
+enum ShapeType { triangle, rectangle }
+
+abstract class Shape {
+  factory Shape(ShapeType type) {
+    switch (type) {
+      case ShapeType.triangle:
+        return Triangle();
+      case ShapeType.rectangle:
+        return Rectangle();
+    }
+  }
+
+  void draw();
+}
+
+class Triangle implements Shape {
+  @override
+  void draw() {
+    print("TRIANGLE");
+  }
+}
+
+class Rectangle implements Shape {
+  @override
+  void draw() {
+    print("RECTANGLE");
+  }
+}
+
+void main() {
+  final shape1 = Shape(ShapeType.triangle);
+  final shape2 = Shape(ShapeType.rectangle);
+
+  shape1.draw();
+  shape2.draw();
+}
